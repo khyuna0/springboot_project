b1e15502c1d97d666cdc2f548f388e9221a301d1
khyuna0 <khohyuna0102@gmail.com>
2025-09-05 17:38:51 +0900

    오라클 설정으로 변경, 테스트 추가

--- src/test/java/com/khyuna0/khyuna0board/Test01.java
+++ src/test/java/com/khyuna0/khyuna0board/Test01.java
@@ -4,6 +4,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -46,7 +47,20 @@ public class Test01 {
 		List<Question> allqQuestions = questionRepository.findAll(); // 모든 질문 글 조회
 		assertEquals(2, allqQuestions.size()); // 예상 결과 ( 기댓값 , 실제값 ) 기댓값 = 실제값이 아니면 테스트 실패(오류)
 		
-		allqQuestions.get(0); // 첫 번째 질문 글 가져오기
+		Question question = allqQuestions.get(0); // 첫 번째 질문 글 가져오기
+		assertEquals("SBB가 무엇인가요?", question.getSubject());	
+	}
+
+	@Test
+	@DisplayName("id(기본키)로 조회 테스트")
+	public void testJpa3() {
+		Optional<Question> optional = questionRepository.findById(3); // 기본키 조회
 		
+		if(optional.isPresent()) { // 참이면 기본키 번호가 존재함
+			Question question = optional.get();
+			assertEquals("SBB가 무엇인가요?", question.getSubject());
+		} else {
+			System.out.println("글없음");
+		}
 	}
 }
