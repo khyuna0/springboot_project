d3e7f40cd82b193e81d56a65f172737fbe95f766
khyuna0 <khohyuna0102@gmail.com>
2025-09-08 12:52:23 +0900

    게시판 질문글 상세 내용 보기

--- src/main/java/com/khyuna0/khyuna0board/question/QuestionController.java
+++ src/main/java/com/khyuna0/khyuna0board/question/QuestionController.java
@@ -6,12 +6,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class QuestionController {
 	
+//	@Autowired
+//	private QuestionRepository questionRepository;
+	
 	@Autowired
-	private QuestionRepository questionRepository;
+	private QuestionService questionService;
 	
 	@GetMapping(value = "/") // root 요청 처리
 	public String root(Model model) {
@@ -22,9 +26,18 @@ public class QuestionController {
 	@GetMapping(value = "/question/list") // value 생략 가능
 	//@ResponseBody
 	public String list(Model model) {
-		List<Question> questionlist = questionRepository.findAll();		
+		//List<Question> questionlist = questionRepository.findAll();		
+		List<Question> questionlist = questionService.getList();
 		model.addAttribute("questionList", questionlist);
 		return "question_list";
 	}
-
+	
+	@GetMapping(value = "/question/detail/{id}") // 파라미터 이름 없이 값만 넘어왔을 때 처리
+	public String detail(Model model, @PathVariable("id") Integer id) {
+		// service 에 3을 넣어서 호출
+		Question question = questionService.getQuestion(id);
+		model.addAttribute("question", question);
+		return "question_detail";
+	}
+	
 }
--- src/main/java/com/khyuna0/khyuna0board/question/QuestionService.java
+++ src/main/java/com/khyuna0/khyuna0board/question/QuestionService.java
@@ -1,12 +1,36 @@
 package com.khyuna0.khyuna0board.question;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import groovyjarjarantlr4.v4.parse.ANTLRParser.throwsSpec_return;
 import lombok.RequiredArgsConstructor;
 
+@RequiredArgsConstructor 
 @Service
 public class QuestionService {
 	
+//	@Autowired
+	private final QuestionRepository questionRepository;
+	// @RequiredArgsConstructor에 의해 생성자 방식으로 주입된 questionRepository
 	
+	public List<Question> getList() { // 모든 질문 글 가져오기
+		
+		return questionRepository.findAll();
+	}//
+	
+	public Question getQuestion(Integer id) {
+		Optional<Question> optional = questionRepository.findById(id);
+		
+		if (optional.isPresent()) { // question 반환
+			return optional.get();
+		} else {
+			throw new DataNotFoundException("question not found");
+		}
+		
+	}//
 	
 }
