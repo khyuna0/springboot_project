08728f6efb15d7fe9a743f14f5482201db923eb9
khyuna0 <khohyuna0102@gmail.com>
2025-09-09 11:10:44 +0900

    더미데이터 300개 생성 테스트 케이스 작성

---
+++ src/test/java/com/khyuna0/khyuna0board/DummyDataInputTest.java
@@ -0,0 +1,25 @@
+package com.khyuna0.khyuna0board;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.khyuna0.khyuna0board.question.QuestionService;
+
+@SpringBootTest
+public class DummyDataInputTest {
+	
+	@Autowired
+	private QuestionService questionService;
+	
+	@Test
+	public void testDummy() {
+		
+		for(int i=1; i<=300; i++) {
+			String subject = String.format("테스트 데이터입니다:[%03d]", i );
+			String content = "연습 내용 더미데이터입니다.";
+			questionService.create(subject,content);	
+		}
+		
+	}
+}
