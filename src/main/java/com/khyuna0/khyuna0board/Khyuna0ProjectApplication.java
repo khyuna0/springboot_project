0393842956075963d43e9366efe712a33bd10b62
khyuna0 <khohyuna0102@gmail.com>
2025-09-12 14:10:01 +0900

    어플리케이션 자바 파일 추가

--- src/main/java/com/khyuna0/khyuna0board/Khyuna0ProjectApplication.java
+++ src/main/java/com/khyuna0/khyuna0board/Khyuna0ProjectApplication.java
@@ -2,10 +2,18 @@ package com.khyuna0.khyuna0board;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class Khyuna0ProjectApplication {
+public class Khyuna0ProjectApplication extends SpringBootServletInitializer{
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		// TODO Auto-generated method stub
+		return builder.sources(Khyuna0ProjectApplication.class);
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(Khyuna0ProjectApplication.class, args);
 	}
